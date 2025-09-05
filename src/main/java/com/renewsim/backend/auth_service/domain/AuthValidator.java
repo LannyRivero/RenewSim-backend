2956a3a8fabaef9d53774271c6f178f810e78275
lannyrivero <lannyrivero288@gmail.com>
2025-09-05 11:33:56 +0200

    feat(auth-validator): implement AuthValidator for credential validation and user enablement checks

---
+++ src/main/java/com/renewsim/backend/auth_service/domain/AuthValidator.java
@@ -0,0 +1,27 @@
+package com.renewsim.backend.auth_service.domain;
+
+import org.springframework.stereotype.Component;
+
+import com.renewsim.backend.auth_service.web.dto.AuthRequestDTO;
+import com.renewsim.backend.shared.exception.AuthenticationException;
+
+@Component
+public class AuthValidator {
+    public void validateCredentials(AuthRequestDTO request) {
+        if (request.getUsername() == null || request.getUsername().isBlank()) {
+            throw new AuthenticationException("Username cannot be blank");
+        }
+        if (request.getPassword() == null || request.getPassword().isBlank()) {
+            throw new AuthenticationException("Password cannot be blank");
+
+        }
+    }
+
+    public void calidateUserEnable(boolean enabled) {
+        if (!enabled) {
+            throw new AuthenticationException("User is disabled");
+        }
+
+    }
+
+}
