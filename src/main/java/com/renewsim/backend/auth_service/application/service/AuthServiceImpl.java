76d215b06486a7fd99180cbecd591835f689d113
lannyrivero <lannyrivero288@gmail.com>
2025-09-08 13:59:25 +0200

    refactor(auth-service): remove unused imports from AuthServiceImpl

--- src/main/java/com/renewsim/backend/auth_service/application/service/AuthServiceImpl.java
+++ src/main/java/com/renewsim/backend/auth_service/application/service/AuthServiceImpl.java
@@ -2,12 +2,8 @@ package com.renewsim.backend.auth_service.application.service;
 
 import com.renewsim.backend.auth_service.application.mapper.AuthResponseMapper;
 import com.renewsim.backend.auth_service.application.port.in.AuthUseCase;
-import com.renewsim.backend.auth_service.application.port.out.ScopePolicy;
-import com.renewsim.backend.auth_service.application.port.out.TokenProvider;
 import com.renewsim.backend.auth_service.application.port.out.UserAccountGateway;
 import com.renewsim.backend.auth_service.domain.AuthValidator;
-import com.renewsim.backend.auth_service.domain.AuthenticatedUser;
-import com.renewsim.backend.auth_service.domain.TokenTimeService;
 import com.renewsim.backend.auth_service.web.dto.AuthRequestDTO;
 import com.renewsim.backend.auth_service.web.dto.AuthResponseDTO;
 import com.renewsim.backend.auth_service.web.dto.UserSnapshot;
@@ -20,9 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Instant;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
