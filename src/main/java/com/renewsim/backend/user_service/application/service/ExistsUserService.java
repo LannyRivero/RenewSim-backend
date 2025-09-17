9d3496619261d4cf6883e857482608dbb8e1804a
lannyrivero <lannyrivero288@gmail.com>
2025-09-17 11:14:14 +0200

    feat(user): refactor ExistsUserService to use UserRepositoryPort and improve user existence checks

--- src/main/java/com/renewsim/backend/user_service/application/service/ExistsUserService.java
+++ src/main/java/com/renewsim/backend/user_service/application/service/ExistsUserService.java
@@ -1,21 +1,34 @@
 package com.renewsim.backend.user_service.application.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.renewsim.backend.user_service.application.port.in.ExistsUserUseCase;
-import com.renewsim.backend.user_service.application.port.out.ExistsUserPort;
+import com.renewsim.backend.user_service.application.port.out.UserRepositoryPort;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ExistsUserService implements ExistsUserUseCase {
-    private final ExistsUserPort existsUserPort;
 
+    private final UserRepositoryPort userRepositoryPort;
+
+    @Override
+    public boolean exists(Long id) {
+        return userRepositoryPort.existsById(id);
+    }
 
     @Override
     public boolean existsByUsernameOrEmail(String username, String email) {
-        return existsUserPort.existsByUsernameOrEmail(username, email);
+        if (username != null) {
+            return userRepositoryPort.existsByUsername(username);
+        }
+        if (email != null) {
+            return userRepositoryPort.existsByEmail(email);
+        }
+        throw new IllegalArgumentException("Either username or email must be provided");
     }
-
 }
+
