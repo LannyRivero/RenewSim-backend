0b5193b5af3cc22013a44b43ed9ccb3b4a6d9009
lannyrivero <lannyrivero288@gmail.com>
2025-09-23 13:11:25 +0200

    feat(role): add null check for Role name in constructor to prevent IllegalArgumentException

--- src/main/java/com/renewsim/backend/role_service/domain/model/Role.java
+++ src/main/java/com/renewsim/backend/role_service/domain/model/Role.java
@@ -4,10 +4,15 @@ import org.springframework.security.core.GrantedAuthority;
 
 public record Role(Long id, RoleName name) implements GrantedAuthority {
 
+    public Role {
+        if (name == null) {
+            throw new IllegalArgumentException("Role name cannot be null");
+
+        }
+    }
+
     @Override
     public String getAuthority() {
         return name.name();
     }
 }
-
-
