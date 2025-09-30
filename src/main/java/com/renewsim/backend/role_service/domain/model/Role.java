ce7793ad360d555db6807cae9141d35ba479fc4f
lannyrivero <lannyrivero288@gmail.com>
2025-09-30 16:28:04 +0200

    feat(role): add validation for Role ID to ensure it is positive if provided

--- src/main/java/com/renewsim/backend/role_service/domain/model/Role.java
+++ src/main/java/com/renewsim/backend/role_service/domain/model/Role.java
@@ -5,6 +5,9 @@ import org.springframework.security.core.GrantedAuthority;
 public record Role(Long id, RoleName name) implements GrantedAuthority {
 
     public Role {
+        if(id != null && id <= 0) {
+            throw new IllegalArgumentException("Role ID must be positive if provided");
+        }
         if (name == null) {
             throw new IllegalArgumentException("RoleName cannot be null");
 
