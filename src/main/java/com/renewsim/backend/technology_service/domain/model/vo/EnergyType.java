08c8a496f6ca8b2ab8856f284371c5c8244eb43f
lannyrivero <lannyrivero288@gmail.com>
2025-10-14 16:21:08 +0200

    feat(enum): define EnergyType enum for technology classification

---
+++ src/main/java/com/renewsim/backend/technology_service/domain/model/vo/EnergyType.java
@@ -0,0 +1,13 @@
+package com.renewsim.backend.technology_service.domain.model.vo;
+
+/**
+ * Enum representing supported renewable energy sources.
+ */
+public enum EnergyType {
+    SOLAR,
+    EOLIC,
+    HYDRO,
+    GEOTHERMAL,
+    BIOMASS
+}
+
