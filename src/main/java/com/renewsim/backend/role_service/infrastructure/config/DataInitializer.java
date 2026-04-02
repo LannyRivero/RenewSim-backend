f5943db7667be1edf7ea510b6379cd6009ee0063
lannyrivero <lannyrivero288@gmail.com>
2026-04-02 12:05:13 +0200

    feat: update DataInitializer to change profile from dev to local for improved environment configuration

--- src/main/java/com/renewsim/backend/role_service/infrastructure/config/DataInitializer.java
+++ src/main/java/com/renewsim/backend/role_service/infrastructure/config/DataInitializer.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile({"dev", "test"})
+@Profile({"local", "test"})
 @RequiredArgsConstructor
 public class DataInitializer {
 
