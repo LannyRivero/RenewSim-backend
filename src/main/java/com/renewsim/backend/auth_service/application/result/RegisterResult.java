873a5757724b4ec4987548bcaf956000660c4240
LannyRivero <lannyrivero288@gmail.com>
2026-04-23 20:56:04 +0200

    feat: refactor RegisterResult to use record instead of class

--- src/main/java/com/renewsim/backend/auth_service/application/result/RegisterResult.java
+++ src/main/java/com/renewsim/backend/auth_service/application/result/RegisterResult.java
@@ -2,17 +2,4 @@ package com.renewsim.backend.auth_service.application.result;
 
 import com.renewsim.backend.user_service.domain.model.UserStatus;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class RegisterResult {
-    private Long id;
-    private String email;
-    private String fullName;
-    private UserStatus status;
-    private String message;
-}
+public record RegisterResult(Long id, String email, String fullName, UserStatus status, String message) {}
