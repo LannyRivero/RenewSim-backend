22c4c92afe66fdb00799bd039c66cbb49bf46be5
LannyRivero <lannyrivero288@gmail.com>
2026-04-23 20:53:19 +0200

    feat: refactor RegisterCommand to use record instead of class

--- src/main/java/com/renewsim/backend/auth_service/application/command/RegisterCommand.java
+++ src/main/java/com/renewsim/backend/auth_service/application/command/RegisterCommand.java
@@ -1,14 +1,3 @@
 package com.renewsim.backend.auth_service.application.command;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class RegisterCommand {
-    private String fullName;
-    private String password;
-    private String email;
-}
+public record RegisterCommand(String fullName, String password, String email) {}
