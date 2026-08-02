83c84ae53f4cd4d81c7d03ef03209b6fc9aff4b6
LannyRivero <lannyrivero288@gmail.com>
2026-08-02 22:44:39 +0200

    refactor(simulation): harden aggregate invariants

---
+++ src/main/java/com/renewsim/backend/simulation_service/domain/exception/InvalidSimulationCreatorException.java
@@ -0,0 +1,8 @@
+package com.renewsim.backend.simulation_service.domain.exception;
+
+public class InvalidSimulationCreatorException extends RuntimeException {
+
+    public InvalidSimulationCreatorException(String message) {
+        super(message);
+    }
+}
