d54c98b61ffecd335a7d3326afa2d3a9a1b07240
LannyRivero <lannyrivero288@gmail.com>
2026-08-18 13:12:32 +0200

    fix(tests): remove unused anyLong import in UpdateSimulationControllerTest

--- src/test/java/com/renewsim/backend/simulation_service/update/web/UpdateSimulationControllerTest.java
+++ src/test/java/com/renewsim/backend/simulation_service/update/web/UpdateSimulationControllerTest.java
@@ -33,7 +33,6 @@ import java.util.Set;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
